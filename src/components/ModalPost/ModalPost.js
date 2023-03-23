532936f2285ee2e351dcd3ef0137d24215a2be43
Aureana <aureana2018@outlook.com>
2023-03-23 12:47:04 -0300

    Finalizando

--- src/components/ModalPost/ModalPost.js
+++ src/components/ModalPost/ModalPost.js
@@ -18,30 +18,11 @@ function ModalPost(props) {
     const [content, setContent] = useState('')
     const { urlPost } = context
 
-    useEffect(() => {
-        browserPost()
+    useEffect(() => { //roda na primeira renderização
         getPostById()
         getCommentsByPostId()
-        //console.log(post)   
-    },
-        [])
+    }, []) 
 
-
-    const browserPost = async () => {
-        try {
-            let auxPost = ''
-            const response = await axios.get(`${BASE_URL}/posts/${urlPost}`, {
-                headers: {
-                    Authorization: window.localStorage.getItem("token")
-                }
-            })
-            auxPost = response.data[0]
-            // console.log("Post", response.data[0])
-            setPost(auxPost)
-        } catch (error) {
-            console.log(error)
-        }
-    }
 
     const getPostById = async () => {
         //renderiza as publicações.
@@ -52,8 +33,6 @@ function ModalPost(props) {
                     Authorization: window.localStorage.getItem("token")
                 }
             })
-            //context.setPosts(response.data)
-            //context.setLoading(false)
 
             const get = response.data.filter((post) => {
 
@@ -61,8 +40,7 @@ function ModalPost(props) {
 
             })
             setPostByID(get[0])
-            //console.log(postByID)
-
+           
 
         } catch (error) {
             console.log(error)
@@ -71,47 +49,32 @@ function ModalPost(props) {
     }
 
 
-    const likePost = async (postId) => {
+    const likeOrDislike = async (id, likeDislike, path) => { //para like e dislike tanto p post como p comentários
         try {
             let body = {
-                like: 1,
+                like: likeDislike
             }
-            await axios.put(`${BASE_URL}/posts/${postId}/like`, body, {
+            await axios.put(`${BASE_URL}/${path}/${id}/like`, body, {
                 headers: {
                     Authorization: window.localStorage.getItem("token")
                 }
             })
-            browserPost()
-            props.browserPosts()
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-
-    const dislikePost = async (postId) => {
-        try {
-            let body = {
-                like: 0,
+            if (path === "posts") {
+                getPostById()
+            } else {
+                getCommentsByPostId()
             }
-            await axios.put(`${BASE_URL}/posts/${postId}/like`, body, {
-                headers: {
-                    Authorization: window.localStorage.getItem("token")
-                }
-            })
-            browserPost()
-            props.browserPosts()
+
         } catch (error) {
             console.log(error)
         }
     }
 
     const insertOtherPost = async () => {
-        // console.log(`${BASE_URL}/comments/${urlPost}`)
+       
         try {
             let body = {
-                content
-
+                comments: content
             }
             await axios.post(`${BASE_URL}/comments/${urlPost}`, body, {
                 headers: {
@@ -119,8 +82,7 @@ function ModalPost(props) {
                 }
             })
             setContent('')
-            browserPost()
-            props.browserPosts()
+            getCommentsByPostId()
 
         } catch (error) {
             console.log(error)
@@ -140,19 +102,11 @@ function ModalPost(props) {
             for (const comment of response.data) {
                 if (comment.postId === context.urlPost) {
                     commentByPostId.push(comment)
-
                 }
-
             }
+
             setComments(commentByPostId)
-            //   const get = response.data.filter((comments)=>{
-            //     console.log(context.urlPost)
 
-
-            //})
-            //setComments(get)
-            //    setPostByID(get[0])
-            //console.log(comments)
 
         } catch (error) {
             console.log(error)
@@ -172,13 +126,13 @@ function ModalPost(props) {
                             <p>{postByID.content}</p>
                             <p className="menuDoPost">
                                 <span className="subTextBold">
-                                    <img src={like} onClick={() => likePost(post.id)} alt="botão-like" />
+                                    <img src={like} onClick={() => likeOrDislike(postByID.id, true, "posts")} alt="botão-like" />
                                     {postByID.likes}
-                                    <img src={dislike} onClick={() => dislikePost(post.id)} alt="botão-dislike" />
+                                    <img src={dislike} onClick={() => likeOrDislike(postByID.id, false, "posts")} alt="botão-dislike" />
                                 </span>
                                 <span className="subText">
                                     <img src={comment} alt="botãoComentários" />
-                                    {post.comments}
+                                    {comments.length}
 
                                 </span>
                             </p>
@@ -188,29 +142,26 @@ function ModalPost(props) {
                     </div>
 
 
-
-
                     {/* comentários da publicação */}
-                    <div>
-                        {/* {post && post?.comments_post?.map((comment) => { */}
+                    <div className="comments">
                         {comments.map((comment) => {
 
                             return (
                                 <article key={comment.id}>
                                     {/* puxar o nome do criador do comentário */}
                                     <p className="subText">Enviado por: {comment.userName}</p>
-                                    {console.log("creator", comment)}
                                     <p>{comment.comments}</p>
                                     <p className="menuDoPost">
                                         <span className="subTextBold">
-                                            <img src={like} onClick={() => likePost(comment.id)} alt="botão-like" />
+                                            <img src={like} onClick={() => likeOrDislike(comment.id, true, "comments")} alt="botão-like" />
                                             {comment.likes}
-                                            <img src={dislike} onClick={() => dislikePost(comment.id)} alt="botão-dislike" />
+                                            <img src={dislike} onClick={() => likeOrDislike(comment.id, false, "comments")} alt="botão-dislike" />
                                         </span>
                                     </p>
                                 </article>
                             )
-                        })}
+                        }).reverse()}
+                        {/* os comentários aparecem em cima */}
                     </div>
                 </StyleSection>
             </StyleContainerModal>
